d5cad61730d0b0121494652b4a80681d00059dd4
DavidW <williams8367@saskpolytech.ca>
2023-06-14 14:37:34 -0600

    nav bar links and temp logo

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-primary bg-dark">
-        <a class="navbar-brand" href="#">Some Logo</a>
+        <router-link class="navbar-brand logo" to="/"><img class="logo" src="../../public/temp-logo.png" alt="temp-logo" /></router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Skills</a>
+              <router-link class="nav-link" to="/skills">Skills</router-link>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Career</a>
@@ -19,3 +19,9 @@
         </div>
       </nav>
 </template>
+
+<style scoped>
+.logo {
+  width: 200px;
+}
+</style>
